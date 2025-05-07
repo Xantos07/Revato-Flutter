96600b760ed2ddad0cc80322bd79c0ea15d66a2b
Xantos07 <coalstudio7@gmail.com>
2025-05-07 15:59:51 +0200

    🚀 Auto deployement add workflow GitHub Actions

--- app/lib/controller/config.dart
+++ app/lib/controller/config.dart
@@ -1,4 +1,5 @@
 const String API_BASE_URL = String.fromEnvironment(
   'API_URL',
-  defaultValue: 'http://localhost:8000',
+  defaultValue: 'http://185.157.247.17:8000',
+  //defaultValue: 'http://localhost:8000',
 );
